09793db60dba253b55a55b366081edb81baa8fd9
Arun Prakash <arun.prakash.456789@gmail.com>
2021-05-27 20:07:54 +0530

    initial commit

---
+++ AutoLogOff/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace AutoLogOff
+{
+	class Program
+	{
+        [DllImport("wtsapi32.dll", SetLastError = true)]
+        static extern bool WTSDisconnectSession(IntPtr hServer, int sessionId, bool bWait);
+
+        const int WTS_CURRENT_SESSION = -1;
+        static readonly IntPtr WTS_CURRENT_SERVER_HANDLE = IntPtr.Zero;
+
+        static void Main(string[] args)
+        {
+            if (!WTSDisconnectSession(WTS_CURRENT_SERVER_HANDLE,
+                 WTS_CURRENT_SESSION, false))
+                throw new Win32Exception();
+        }
+    }
+}
